5333bfec297b4b2afac7a66154897ae27e742e81
Thiago Bastos <tbastos@motorola.com>
2020-08-07 18:57:13 -0300

    1.3 Build your first notification
    It just makes the notification appears
    but it doesn't do anything.

--- app/src/main/java/com/tab/notifyme/MainActivity.kt
+++ app/src/main/java/com/tab/notifyme/MainActivity.kt
@@ -7,18 +7,20 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.tab.notifyme.databinding.ActivityMainBinding
 
-const val PRIMARI_CHANNEL_ID: String = "primary_notification_channel"
+const val PRIMARY_CHANNEL_ID: String = "primary_notification_channel"
+const val NOTIFICATION_ID: Int = 0
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mNotifyManager: NotificationManager
 
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var button_notify: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,21 +28,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        createNotificationChannel()
+
         button_notify = binding.notify
-        button_notify.setOnClickListener({ view -> sendNotification() })
+        button_notify.setOnClickListener { view -> sendNotification() }
     }
 
     private fun createNotificationChannel() {
         mNotifyManager = getSystemService(
-                Context.NOTIFICATION_SERVICE) as NotificationManager
+            Context.NOTIFICATION_SERVICE) as NotificationManager
         //Notification Channels are only available in API 26 of higher
         if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.O) {
+            >= Build.VERSION_CODES.O) {
             // Create NotificationChannel
-            var notificationChannel = NotificationChannel(
-                    PRIMARI_CHANNEL_ID,
-                    getString(R.string.noti_channel_name_mascot),
-                    NotificationManager.IMPORTANCE_HIGH)
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                getString(R.string.noti_channel_name_mascot),
+                NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(true);
@@ -49,8 +53,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getNotificationBuilder(): NotificationCompat.Builder =
+        NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("You've been notified!")
+            .setContentText("This is your notification text.")
+            .setSmallIcon(R.drawable.ic_android)
+
     private fun sendNotification() {
-        // TO-DO
+        mNotifyManager.notify(NOTIFICATION_ID, getNotificationBuilder().build())
+        Toast.makeText(this, "pi", Toast.LENGTH_LONG).show()
     }
 
 }
